1ea03fb3052f587850a0c047160d89d9186f0ccf
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2020-03-01 13:08:16 +0100

    Update minecraft-vanilla-server-hibernation.py

--- minecraft-vanilla-server-hibernation.py
+++ minecraft-vanilla-server-hibernation.py
@@ -20,9 +20,9 @@ LISTEN_PORT = 25565         #the port you will connect to on minecraft client
 TARGET_HOST = "127.0.0.1"
 TARGET_PORT = 25555         #the port specified on server.properties
 
-MINECRAFT_SERVER_STARTUPTIME = 120 # time the server needs until it is fully started
+MINECRAFT_SERVER_STARTUPTIME = 30 # time the server needs until it is fully started
 
-TIME_BEFORE_STOPPING_EMPTY_SERVER = 240 
+TIME_BEFORE_STOPPING_EMPTY_SERVER = 120 
 
 #-----------------------advanced------------------------------#
 
