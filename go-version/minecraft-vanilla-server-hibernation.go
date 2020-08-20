18aca2d6e2b9ea28c077b24bc6e4220570d001b5
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2020-08-20 21:15:36 +0200

    multiple features update
    removed FindPattern function since it works only for some specific player name cases (of lenght 9)
    renamed variables
    added logger
    more easily readable program layout
    removed not useful functions

--- go-version/minecraft-vanilla-server-hibernation.go
+++ go-version/minecraft-vanilla-server-hibernation.go
@@ -1,10 +1,3 @@
-/*
-minecraft-vanilla_server_hibernation is used to start and stop automatically a vanilla minecraft server
-Copyright (C) 2020  gekigek99
-v1.3 (Go)
-visit my github page: https://github.com/gekigek99
-*/
-
 package main
 
 import (
@@ -20,51 +13,62 @@ import (
 	"time"
 )
 
-//------------------------modify-------------------------------//
+var info []string = []string{
+	"Minecraft-Vanilla-Server-Hibernation is used to auto-start/stop a vanilla minecraft server",
+	"Copyright (C) 2019-2020 gekigek99",
+	"v1.4 (Go)",
+	"visit my github page: https://github.com/gekigek99",
+	"if you like what I do please consider having a cup of coffee with me at: https://www.buymeacoffee.com/gekigek99",
+}
 
-var startminecraftserverwin = []string{"java", "-Xmx1024M", "-Xms1024M", "-jar", "server.jar", "nogui"}
+//---------------------------modify---------------------------//
 
-const stopminecraftserverwin = "stop"
-const startminecraftserver = "systemctl start minecraft-server"
-const stopminecraftserver = "systemctl stop minecraft-server"
+const startMinecraftServerLin = "systemctl start minecraft-server"
+const stopMinecraftServerLin = "systemctl stop minecraft-server"
+const startMinecraftServerWin = "java -Xmx1024M -Xms1024M -jar server.jar nogui"
+const stopMinecraftServerWin = "stop"
 
-const minecraftserverstartuptime = 20
-const timebeforestoppingemptyserver = 60
+const minecraftServerStartupTime = 20
+const timeBeforeStoppingEmptyServer = 60
 
-//-----------------------advanced------------------------------//
+//--------------------------advanced--------------------------//
 
-const listenhost = "0.0.0.0"
-const listenport = "25555"
+const listenHost = "0.0.0.0"
+const listenPort = "25555"
 
-const targethost = "127.0.0.1"
-const targetport = "25565"
+const targetHost = "127.0.0.1"
+const targetPort = "25565"
 
 const debug = false
 
-//---------------------do not modify---------------------------//
+//------------------------don't modify------------------------//
 
 var players int = 0
-var datacountbytestoserver, datacountbytestoclients float64 = 0, 0
-var serverstatus string = "offline"
-var timelefttillup int = minecraftserverstartuptime
-var stopinstances int = 0
+var dataCountBytesToServer, dataCountBytesToClients float64 = 0, 0
+var serverStatus string = "offline"
+var timeLeftUntilTp int = minecraftServerStartupTime
+var stopInstances int = 0
 var mutex = &sync.Mutex{}
+
+//------------------------go specific-------------------------//
+
 var cmdIn io.WriteCloser
 
-//StartMinecraftServer starts the minecraft server
-func StartMinecraftServer() {
-	if serverstatus != "offline" {
+//--------------------------PROGRAM---------------------------//
+
+func startMinecraftServer() {
+	if serverStatus != "offline" {
 		return
 	}
-	serverstatus = "starting"
+	serverStatus = "starting"
 
 	if runtime.GOOS == "linux" {
-		err := exec.Command("/bin/bash", "-c", startminecraftserver).Run()
+		err := exec.Command("/bin/bash", "-c", startMinecraftServerLin).Run()
 		if err != nil {
 			log.Printf("error starting minecraft server: %v\n", err)
 		}
 	} else if runtime.GOOS == "windows" {
-		cmd := exec.Command(startminecraftserverwin[0], startminecraftserverwin[1:]...)
+		cmd := exec.Command(strings.Split(startMinecraftServerWin, " ")[0], strings.Split(startMinecraftServerWin, " ")[1:]...)
 		cmdIn, _ = cmd.StdinPipe()
 		cmd.Start()
 	} else {
@@ -74,28 +78,27 @@ func StartMinecraftServer() {
 
 	log.Print("*** MINECRAFT SERVER IS STARTING!")
 	players = 0
-	UpdateTimeleft()
-	go Timer(minecraftserverstartuptime, SetServerStatusOnline)
+	updateTimeleft()
+	go timer(minecraftServerStartupTime, setServerStatusOnline)
 }
 
-//StopEmptyMinecraftServer stops the minecraft server
-func StopEmptyMinecraftServer() {
+func stopEmptyMinecraftServer() {
 	mutex.Lock()
-	stopinstances--
-	if stopinstances > 0 || players > 0 || serverstatus == "offline" {
+	stopInstances--
+	if stopInstances > 0 || players > 0 || serverStatus == "offline" {
 		mutex.Unlock()
 		return
 	}
 	mutex.Unlock()
-	serverstatus = "offline"
+	serverStatus = "offline"
 
 	if runtime.GOOS == "linux" {
-		err := exec.Command("/bin/bash", "-c", stopminecraftserver).Run()
+		err := exec.Command("/bin/bash", "-c", stopMinecraftServerLin).Run()
 		if err != nil {
 			log.Printf("error stopping minecraft server: %v\n", err)
 		}
 	} else if runtime.GOOS == "windows" {
-		cmdIn.Write([]byte(stopminecraftserverwin))
+		cmdIn.Write([]byte(stopMinecraftServerWin))
 		cmdIn.Close()
 	} else {
 		log.Print("error: OS not supported!")
@@ -103,188 +106,169 @@ func StopEmptyMinecraftServer() {
 	}
 
 	log.Print("*** MINECRAFT SERVER IS SHUTTING DOWN!")
-	timelefttillup = minecraftserverstartuptime
+	timeLeftUntilTp = minecraftServerStartupTime
 }
 
-//SetServerStatusOnline sets the server status online
-func SetServerStatusOnline() {
-	serverstatus = "online"
+func setServerStatusOnline() {
+	serverStatus = "online"
 	log.Print("*** MINECRAFT SERVER IS UP!")
 	mutex.Lock()
-	stopinstances++
+	stopInstances++
 	mutex.Unlock()
-	go Timer(timebeforestoppingemptyserver, StopEmptyMinecraftServer)
+	go timer(timeBeforeStoppingEmptyServer, stopEmptyMinecraftServer)
 }
 
-//UpdateTimeleft updates the global variable timelefttillup
-func UpdateTimeleft() {
-	if timelefttillup > 0 {
-		timelefttillup--
-		go Timer(1, UpdateTimeleft)
+func updateTimeleft() {
+	if timeLeftUntilTp > 0 {
+		timeLeftUntilTp--
+		go timer(1, updateTimeleft)
 	}
 }
 
-func printdatausage() {
-	if debug == true {
-		mutex.Lock()
-		if datacountbytestoserver != 0 || datacountbytestoclients != 0 {
-			log.Printf("data/s: %8.3f KB/s to clients | %8.3f KB/s to server\n", datacountbytestoclients/1024, datacountbytestoserver/1024)
-			datacountbytestoserver = 0
-			datacountbytestoclients = 0
-		}
-		mutex.Unlock()
-		go Timer(1, printdatausage)
+func printDataUsage() {
+	mutex.Lock()
+	if dataCountBytesToServer != 0 || dataCountBytesToClients != 0 {
+		logger(fmt.Sprintf("data/s: %8.3f KB/s to clients | %8.3f KB/s to server\n", dataCountBytesToClients/1024, dataCountBytesToServer/1024))
+		dataCountBytesToServer = 0
+		dataCountBytesToClients = 0
 	}
+	mutex.Unlock()
+	go timer(1, printDataUsage)
 }
 
 func main() {
-	fmt.Println("minecraft-vanilla-server-hibernation v1.1 (Go)")
-	fmt.Println("Copyright (C) 2020 gekigek99")
-	fmt.Println("visit my github page for updates: https://github.com/gekigek99")
+	fmt.Println(strings.Join(info[1:4], "\n"))
 	for {
-		docksocket, err := net.Listen("tcp", listenhost+":"+listenport)
-		CheckError(err)
+		dockSocket, err := net.Listen("tcp", listenHost+":"+listenPort)
+		checkError(err)
 		defer func() {
-			if debug == true {
-				log.Println("Closing connection for: docksocket")
-			}
-			docksocket.Close()
+			logger("Closing connection for: dockSocket")
+			dockSocket.Close()
 		}()
 		log.Println("*** listening for new clients to connect...")
-		printdatausage()
+		printDataUsage()
 		for {
-			clientsocket, err := docksocket.Accept()
+			clientSocket, err := dockSocket.Accept()
 			if err != nil {
 				continue
 			}
-			go handleclientsocket(clientsocket)
+			go handleClientSocket(clientSocket)
 		}
 	}
 
 }
 
-func handleclientsocket(clientsocket net.Conn) {
-	clientsocketremoteaddr := strings.Split(clientsocket.RemoteAddr().String(), ":")[0]
-	if serverstatus == "offline" || serverstatus == "starting" {
+func handleClientSocket(clientSocket net.Conn) {
+	clientAddress := strings.Split(clientSocket.RemoteAddr().String(), ":")[0]
+	logger(fmt.Sprintf("*** from %s:%s to %s:%s", clientAddress, listenPort, targetHost, targetPort))
+	if serverStatus == "offline" || serverStatus == "starting" {
 		buffer := make([]byte, 1024)
-		datalenght, err := clientsocket.Read(buffer)
+		dataLenght, err := clientSocket.Read(buffer)
 		if err != nil {
-			if debug == true {
-				log.Printf("error during clientsocket.Read() 1")
-			}
+			logger("error during clientSocket.Read() 1")
 			return
 		}
-		locationpattern, foundpattern := FindPattern(buffer[:datalenght], []byte{2, 11, 0, 9}) //for me is 2,11,0,9 don't know if it's a general rule
-		if buffer[datalenght-1] == 2 || foundpattern {
-			playername := ""
-			if foundpattern == false {
-				datalenght, err = clientsocket.Read(buffer)
-				if err != nil {
-					if debug == true {
-						log.Printf("error during clientsocket.Read() 2")
-					}
-					return
-				}
-				playername = string(buffer[3:datalenght])
-			} else {
-				playername = string(buffer[locationpattern+4 : datalenght])
+
+		if buffer[dataLenght-1] == 1 {
+			if serverStatus == "offline" {
+				log.Printf("*** player unknown requested server info from %s:%s to %s:%s\n", clientAddress, listenPort, targetHost, targetPort)
+			} else if serverStatus == "starting" {
+				log.Printf("*** player unknown requested server info from %s:%s to %s:%s during server startup\n", clientAddress, listenPort, targetHost, targetPort)
 			}
-			if serverstatus == "offline" {
-				log.Printf("*** %s tryed to join from %s:%s to %s:%s\n", playername, clientsocketremoteaddr, listenport, targethost, targetport)
-				StartMinecraftServer()
+
+		} else if buffer[dataLenght-1] == 2 {
+			dataLenght, err = clientSocket.Read(buffer)
+			if err != nil {
+				logger("error during clientSocket.Read() 2")
+				return
 			}
-			if serverstatus == "starting" {
-				log.Printf("*** %s tryed to join from %s:%s to %s:%s during server startup\n", playername, clientsocketremoteaddr, listenport, targethost, targetport)
-				message := BuildMessage(fmt.Sprintf("Server is starting. Please wait. Time left: %d seconds", timelefttillup))
-				clientsocket.Write(message)
-			}
-		} else {
-			locationpattern, foundpattern := FindPattern(buffer[:datalenght], []byte{1, 1, 0})
-			if buffer[datalenght-1] == 1 || (locationpattern == datalenght-3 && foundpattern) { //sometimes the requests ends with  [...1 1 0] and not with [...1]
-				if serverstatus == "offline" {
-					log.Printf("*** player unknown requested server info from %s:%s to %s:%s\n", clientsocketremoteaddr, listenport, targethost, targetport)
-				}
-				if serverstatus == "starting" {
-					log.Printf("*** player unknown requested server info from %s:%s to %s:%s during server startup\n", clientsocketremoteaddr, listenport, targethost, targetport)
-				}
+			playerName := string(buffer[3:dataLenght])
+
+			if serverStatus == "offline" {
+				startMinecraftServer()
+				log.Printf("*** %s tryed to join from %s:%s to %s:%s\n", playerName, clientAddress, listenPort, targetHost, targetPort)
+				clientSocket.Write(buildMessage(fmt.Sprintf("Server start command issued. Please wait... Time left: %d seconds", timeLeftUntilTp)))
+			} else if serverStatus == "starting" {
+				log.Printf("*** %s tryed to join from %s:%s to %s:%s during server startup\n", playerName, clientAddress, listenPort, targetHost, targetPort)
+				clientSocket.Write(buildMessage(fmt.Sprintf("Server is starting. Please wait... Time left: %d seconds", timeLeftUntilTp)))
 			}
 		}
-		if debug == true {
-			log.Println("closing connection for: ", clientsocketremoteaddr)
-		}
-		clientsocket.Close()
+
+		logger(fmt.Sprintf("closing connection for: %s", clientAddress))
+		clientSocket.Close()
 	}
-	if serverstatus == "online" {
-		serversocket, err := net.Dial("tcp", targethost+":"+targetport)
+
+	if serverStatus == "online" {
+		serverSocket, err := net.Dial("tcp", targetHost+":"+targetPort)
 		if err != nil {
-			if debug == true {
-				log.Printf("error during serversocket.Dial()")
-			}
+			logger("error during serverSocket.Dial()")
 			return
 		}
-		connectsocketsasync(clientsocket, serversocket)
+
+		connectSocketsAsync(clientSocket, serverSocket)
 	}
 }
 
-func connectsocketsasync(client net.Conn, server net.Conn) {
-	go ClientToServer(client, server)
-	go ServerToClient(server, client)
+func connectSocketsAsync(client net.Conn, server net.Conn) {
+	go clientToServer(client, server)
+	go serverToClient(server, client)
 }
 
-//ClientToServer manages the client to server connection
-func ClientToServer(source, destination net.Conn) {
+func clientToServer(source, destination net.Conn) {
 	players++
 	log.Printf("*** A PLAYER JOINED THE SERVER! - %d players online", players)
-	ForwardSync(source, destination, false)
+
+	forwardSync(source, destination, false)
+
 	players--
 	log.Printf("*** A PLAYER LEFT THE SERVER! - %d players online", players)
+
 	mutex.Lock()
-	stopinstances++
+	stopInstances++
 	mutex.Unlock()
-	go Timer(timebeforestoppingemptyserver, StopEmptyMinecraftServer)
+
+	go timer(timeBeforeStoppingEmptyServer, stopEmptyMinecraftServer)
 }
 
-//ServerToClient manages the server to client connection
-func ServerToClient(source, destination net.Conn) {
-	ForwardSync(source, destination, true)
+func serverToClient(source, destination net.Conn) {
+	forwardSync(source, destination, true)
 }
 
-//ForwardSync takes a source and a destination net.Conn and forwards them (plus takes a true or false to know what it is forwarding)
-func ForwardSync(source, destination net.Conn, servertoclient bool) {
+//forwardSync takes a source and a destination net.Conn and forwards them (plus takes a true or false to know what it is forwarding)
+func forwardSync(source, destination net.Conn, isServerToClient bool) {
 	data := make([]byte, 1024)
 	for {
-		source.SetReadDeadline(time.Now().Add(20 * time.Second))
-		destination.SetWriteDeadline(time.Now().Add(20 * time.Second))
-		lenghtdata, err := source.Read(data)
+		source.SetReadDeadline(time.Now().Add(timeBeforeStoppingEmptyServer * time.Second))
+		destination.SetWriteDeadline(time.Now().Add(timeBeforeStoppingEmptyServer * time.Second))
+
+		dataLen, err := source.Read(data)
 		if err != nil {
-			if debug == true {
-				if err == io.EOF {
-					log.Printf("closing %s --> %s because of EOF", strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0])
-				} else {
-					log.Printf("error in forward(): %v\nsource: %s\ndestination: %s", err, strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0])
-				}
+			if err == io.EOF {
+				logger(fmt.Sprintf("closing %s --> %s because of EOF", strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0]))
+			} else {
+				logger(fmt.Sprintf("error in forward(): %v\nsource: %s\ndestination: %s", err, strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0]))
 			}
 			source.Close()
 			destination.Close()
 			break
 		}
-		destination.Write(data[:lenghtdata])
-		if debug == true {
+		destination.Write(data[:dataLen])
+
+		if debug {
 			mutex.Lock()
-			if servertoclient == true {
-				datacountbytestoclients = datacountbytestoclients + float64(lenghtdata)
+			if isServerToClient {
+				dataCountBytesToClients = dataCountBytesToClients + float64(dataLen)
 			} else {
-				datacountbytestoserver = datacountbytestoserver + float64(lenghtdata)
+				dataCountBytesToServer = dataCountBytesToServer + float64(dataLen)
 			}
 			mutex.Unlock()
 		}
 	}
 }
 
-//----------------------func tools-----------------------------//
+//---------------------------utils----------------------------//
 
-//CheckError takes an error variable and exit if not nil
-func CheckError(err error) {
+func checkError(err error) {
 	if err != nil {
 		log.Printf("Fatal error: %s", err.Error())
 		time.Sleep(time.Duration(5) * time.Second)
@@ -292,40 +276,20 @@ func CheckError(err error) {
 	}
 }
 
-//Timer takes a time interval and execute a function after that time has passed
-func Timer(timeleft int, f func()) {
+//timer takes a time interval and execute a function after that time has passed
+func timer(timeleft int, f func()) {
 	time.Sleep(time.Duration(timeleft) * time.Second)
 	f()
 }
 
-//BuildMessage takes a string and returns a []byte
-func BuildMessage(message string) []byte {
+func buildMessage(message string) []byte {
 	message = "{\"text\":\"" + message + "\"}"
 	encodedMessage := append([]byte{byte(len(message) + 2), byte(0), byte(len(message))}, []byte(message)...)
 	return encodedMessage
 }
 
-//PaddString takes a string a padding and a lenght and returns a string
-func PaddString(text, padding string, lenght int) string {
-	for i := len(text); i < lenght; i++ {
-		text = text + padding
+func logger(message string) {
+	if debug {
+		log.Println(message)
 	}
-	return text
-}
-
-//FindPattern returns true and the location of the pattern found, or returns false and -1
-func FindPattern(array, pattern []byte) (int, bool) {
-	lenarray := len(array)
-	lenpattern := len(pattern)
-	for i := 0; i <= lenarray-lenpattern; i++ {
-		for j := 0; j < lenpattern; j++ {
-			if array[i+j] != pattern[j] {
-				break
-			}
-			if j == lenpattern-1 {
-				return i, true
-			}
-		}
-	}
-	return -1, false
 }
