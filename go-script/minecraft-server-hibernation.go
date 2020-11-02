d67c4d51245620ac8a986ec672f9ceb4355fc55c
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2020-11-02 02:01:28 +0100

    error correction in buildMessage comment

--- go-script/minecraft-server-hibernation.go
+++ go-script/minecraft-server-hibernation.go
@@ -456,7 +456,7 @@ func buildMessage(format, message string) []byte {
 		// mountHeader: mounts the complete header to a specified message
 		//					┌----------------complete header----------------┐
 		// scheme: 			[sub-header1	|sub-header2 	|sub-header3	|message	]
-		// bytes used:		[2				|1				|2				|0 ... 16384]
+		// bytes used:		[2				|1				|2				|0 ... 16381]
 		// value range:		[131 0 - 255 127	|0				|128 0 - 252 127	|---		]
 
 		var addSubHeader = func(message []byte) []byte {
