f9723a6edf4457aa602f2ddd17280ceb3ce22147
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2021-01-29 22:36:59 +0100

    split script into packages (3)
    new packages:
    - proxy

--- lib/connctrl/connctrl.go
+++ lib/connctrl/connctrl.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"strconv"
@@ -14,6 +13,7 @@ import (
 	"msh/lib/asyncctrl"
 	"msh/lib/confctrl"
 	"msh/lib/debugctrl"
+	"msh/lib/proxy"
 	"msh/lib/servctrl"
 	"msh/lib/servprotocol"
 )
@@ -132,7 +132,7 @@ func clientToServer(source, destination net.Conn, stopSig *bool) {
 	log.Printf("*** A PLAYER JOINED THE SERVER! - %d players online", servctrl.Players)
 
 	// exchanges data from client to server (isServerToClient == false)
-	forwardSync(source, destination, false, stopSig)
+	proxy.Forward(source, destination, false, stopSig)
 
 	servctrl.Players--
 	log.Printf("*** A PLAYER LEFT THE SERVER! - %d players online", servctrl.Players)
@@ -147,89 +147,5 @@ func clientToServer(source, destination net.Conn, stopSig *bool) {
 
 func serverToClient(source, destination net.Conn, stopSig *bool) {
 	// exchanges data from server to client (isServerToClient == true)
-	forwardSync(source, destination, true, stopSig)
-}
-
-// forwardSync takes a source and a destination net.Conn and forwards them.
-// (isServerToClient used to know the forward direction)
-func forwardSync(source, destination net.Conn, isServerToClient bool, stopSig *bool) {
-	data := make([]byte, 1024)
-
-	// set to false after the first for cycle
-	firstBuffer := true
-
-	for {
-		if *stopSig {
-			// if stopSig is true, close the source connection
-			source.Close()
-			break
-		}
-
-		// update read and write timeout
-		source.SetReadDeadline(time.Now().Add(time.Duration(confctrl.Config.Basic.TimeBeforeStoppingEmptyServer) * time.Second))
-		destination.SetWriteDeadline(time.Now().Add(time.Duration(confctrl.Config.Basic.TimeBeforeStoppingEmptyServer) * time.Second))
-
-		// read data from source
-		dataLen, err := source.Read(data)
-		if err != nil {
-			// case in which the connection is closed by the source or closed by target
-			if err == io.EOF {
-				debugctrl.Logger(fmt.Sprintf("closing %s --> %s because of: %s", strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0], err.Error()))
-			} else {
-				debugctrl.Logger(fmt.Sprintf("forwardSync: error in forward(): %v\n%s --> %s", err, strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0]))
-			}
-
-			// close the source connection
-			asyncctrl.Mutex.Lock()
-			*stopSig = true
-			asyncctrl.Mutex.Unlock()
-			source.Close()
-			break
-		}
-
-		// write data to destination
-		destination.Write(data[:dataLen])
-
-		// calculate bytes/s to client/server
-		if confctrl.Config.Advanced.Debug {
-			asyncctrl.Mutex.Lock()
-			if isServerToClient {
-				debugctrl.DataCountBytesToClients = debugctrl.DataCountBytesToClients + float64(dataLen)
-			} else {
-				debugctrl.DataCountBytesToServer = debugctrl.DataCountBytesToServer + float64(dataLen)
-			}
-			asyncctrl.Mutex.Unlock()
-		}
-
-		// version/protocol are only found in serverToClient connection in the first buffer that is read
-		if firstBuffer && isServerToClient {
-			searchVersionProtocol(data[:dataLen])
-
-			// first cycle is finished, set firstBuffer = false
-			firstBuffer = false
-		}
-	}
-}
-
-// searchVersionProtocol finds the serverVersion and serverProtocol and writes them in the config file
-func searchVersionProtocol(data []byte) {
-	// if the above specified buffer contains "\"version\":{\"name\":\"" and ",\"protocol\":" --> extract the serverVersion and serverProtocol
-	if bytes.Contains(data, []byte("\"version\":{\"name\":\"")) && bytes.Contains(data, []byte(",\"protocol\":")) {
-		newServerVersion := string(bytes.Split(bytes.Split(data, []byte("\"version\":{\"name\":\""))[1], []byte("\","))[0])
-		newServerProtocol := string(bytes.Split(bytes.Split(data, []byte(",\"protocol\":"))[1], []byte("}"))[0])
-
-		// if serverVersion or serverProtocol are different from the ones specified in config.json --> update them
-		if newServerVersion != confctrl.Config.Advanced.ServerVersion || newServerProtocol != confctrl.Config.Advanced.ServerProtocol {
-			debugctrl.Logger(
-				"server version found!",
-				"serverVersion:", newServerVersion,
-				"serverProtocol:", newServerProtocol,
-			)
-
-			confctrl.Config.Advanced.ServerVersion = newServerVersion
-			confctrl.Config.Advanced.ServerProtocol = newServerProtocol
-
-			confctrl.SaveConfig()
-		}
-	}
+	proxy.Forward(source, destination, true, stopSig)
 }
---
+++ lib/proxy/proxy.go
@@ -0,0 +1,75 @@
+package proxy
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"time"
+
+	"msh/lib/asyncctrl"
+	"msh/lib/confctrl"
+	"msh/lib/debugctrl"
+	"msh/lib/servprotocol"
+)
+
+// Forward takes a source and a destination net.Conn and forwards them.
+// (isServerToClient used to know the forward direction)
+func Forward(source, destination net.Conn, isServerToClient bool, stopSig *bool) {
+	data := make([]byte, 1024)
+
+	// set to false after the first for cycle
+	firstBuffer := true
+
+	for {
+		if *stopSig {
+			// if stopSig is true, close the source connection
+			source.Close()
+			break
+		}
+
+		// update read and write timeout
+		source.SetReadDeadline(time.Now().Add(time.Duration(confctrl.Config.Basic.TimeBeforeStoppingEmptyServer) * time.Second))
+		destination.SetWriteDeadline(time.Now().Add(time.Duration(confctrl.Config.Basic.TimeBeforeStoppingEmptyServer) * time.Second))
+
+		// read data from source
+		dataLen, err := source.Read(data)
+		if err != nil {
+			// case in which the connection is closed by the source or closed by target
+			if err == io.EOF {
+				debugctrl.Logger(fmt.Sprintf("closing %s --> %s because of: %s", strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0], err.Error()))
+			} else {
+				debugctrl.Logger(fmt.Sprintf("forwardSync: error in forward(): %v\n%s --> %s", err, strings.Split(source.RemoteAddr().String(), ":")[0], strings.Split(destination.RemoteAddr().String(), ":")[0]))
+			}
+
+			// close the source connection
+			asyncctrl.Mutex.Lock()
+			*stopSig = true
+			asyncctrl.Mutex.Unlock()
+			source.Close()
+			break
+		}
+
+		// write data to destination
+		destination.Write(data[:dataLen])
+
+		// calculate bytes/s to client/server
+		if confctrl.Config.Advanced.Debug {
+			asyncctrl.Mutex.Lock()
+			if isServerToClient {
+				debugctrl.DataCountBytesToClients = debugctrl.DataCountBytesToClients + float64(dataLen)
+			} else {
+				debugctrl.DataCountBytesToServer = debugctrl.DataCountBytesToServer + float64(dataLen)
+			}
+			asyncctrl.Mutex.Unlock()
+		}
+
+		// version/protocol are only found in serverToClient connection in the first buffer that is read
+		if firstBuffer && isServerToClient {
+			servprotocol.SearchVersionProtocol(data[:dataLen])
+
+			// first cycle is finished, set firstBuffer = false
+			firstBuffer = false
+		}
+	}
+}
