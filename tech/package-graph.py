32f2634e2d735b41b9432634c07c24b9d3eac9f8
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2023-03-03 11:15:34 +0100

    tech: package-graph: improved execution speed

--- tech/package-graph.py
+++ tech/package-graph.py
@@ -2,6 +2,7 @@ import os
 import graphviz
 import subprocess
 import json
+import time
 
 describe_tag = subprocess.check_output(["git", "describe", "--tags"]).decode("utf-8").strip()
 title = "msh package graph (" + describe_tag + ")"
@@ -14,10 +15,14 @@ graph.graph_attr["labelloc"] = "t"	# title on top
 visitedRoot = {}
 
 def main():
+	timestart = time.time()
+
 	getImports("")
 	print(json.dumps(visitedRoot, indent=2, sort_keys=True))
 
 	graph.render(os.path.join(os.path.realpath(os.path.dirname(__file__)), "package-graph"), format="png", view=True, cleanup=True)
+
+	print("execution time: %.3f seconds" % float(time.time()-timestart))
 
 def getImports(rootAddr: str):
 	# rootAddr:	msh/lib/errco
@@ -26,17 +31,19 @@ def getImports(rootAddr: str):
 		rootName = "main"
 	else:
 		rootName = rootAddr.replace("msh/lib/", "")
+	
+	if rootName in visitedRoot:
+		return
+	else:
+		# initialize list of visited packages so that packages that
+		# don't import others are included in the dict as an empty list
+		visitedRoot[rootName] = []
 
 	packages_string = subprocess.check_output(["go", "list", "-f", "{{ .Imports }}", rootAddr]).decode("utf-8")
 	packages = packages_string.replace("[", "").replace("]", "").split()
 	packagesAddr = [e for e in packages if "msh" in e]
 
 	graph.node(rootName)
-
-	# initialize list of visited packages so that packages that
-	# don't import others are included in the dict as an empty list
-	if rootName not in visitedRoot:
-		visitedRoot[rootName] = []
 	
 	for packageAddr in packagesAddr:
 		if visited(packageAddr, rootName):
