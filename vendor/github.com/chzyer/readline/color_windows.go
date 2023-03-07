56327e5c4dd06dbd146e1e86d15b3e4ad75d50d2
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2023-03-07 02:24:54 +0100

    vendor: updated

--- vendor/github.com/chzyer/readline/color_windows.go
+++ vendor/github.com/chzyer/readline/color_windows.go
@@ -34,42 +34,52 @@ import (
 )
 
 var (
-	ansiSuccess bool
+	ansiErr     error
 	ansiOnce    sync.Once
 )
 
-func enableANSI() bool {
+func enableANSI() error {
 	ansiOnce.Do(func() {
-		ansiSuccess = realEnableANSI()
+		ansiErr = realEnableANSI()
 	})
-	return ansiSuccess
+	return ansiErr
 }
 
-// this is `enableColor` from https://github.com/jwalton/go-supportscolor
-func realEnableANSI() bool {
-	handle, err := windows.GetStdHandle(windows.STD_OUTPUT_HANDLE)
+func realEnableANSI() error {
+	// We want to enable the following modes, if they are not already set:
+	// ENABLE_VIRTUAL_TERMINAL_PROCESSING on stdout (color support)
+	// ENABLE_VIRTUAL_TERMINAL_INPUT on stdin (ansi input sequences)
+	// See https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
+	if err := windowsSetMode(windows.STD_OUTPUT_HANDLE, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING); err != nil {
+		return err
+	}
+	if err := windowsSetMode(windows.STD_INPUT_HANDLE, windows.ENABLE_VIRTUAL_TERMINAL_INPUT); err != nil {
+		return err
+	}
+	return nil
+}
+
+func windowsSetMode(stdhandle uint32, modeFlag uint32) (err error) {
+	handle, err := windows.GetStdHandle(stdhandle)
 	if err != nil {
-		return false
+		return err
 	}
 
 	// Get the existing console mode.
 	var mode uint32
 	err = windows.GetConsoleMode(handle, &mode)
 	if err != nil {
-		return false
+		return err
 	}
 
-	// If ENABLE_VIRTUAL_TERMINAL_PROCESSING is not set, then set it.  This will
-	// enable native ANSI color support from Windows.
-	if mode&windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING != windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING {
-		// Enable color.
-		// See https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences.
-		mode = mode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING
+	// Enable the mode if it is not currently set
+	if mode&modeFlag != modeFlag {
+		mode = mode | modeFlag
 		err = windows.SetConsoleMode(handle, mode)
 		if err != nil {
-			return false
+			return err
 		}
 	}
 
-	return true
+	return nil
 }
