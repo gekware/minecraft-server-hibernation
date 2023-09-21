79e7f115894ba61d1528a69dea0c04f798f3860d
DevMiner <devminer@devminer.xyz>
2023-09-21 19:11:17 +0200

    fix: timeouts during login

--- lib/conn/conn.go
+++ lib/conn/conn.go
@@ -230,8 +230,8 @@ func forwardTCP(source, destination net.Conn, isServerToClient bool, req int) {
 
 	for {
 		// update read and write timeout
-		source.SetReadDeadline(time.Now().Add(10 * time.Second))
-		destination.SetWriteDeadline(time.Now().Add(10 * time.Second))
+		source.SetReadDeadline(time.Now().Add(60 * time.Second))
+		destination.SetWriteDeadline(time.Now().Add(60 * time.Second))
 
 		// read data from source
 		dataLen, err := source.Read(data)
